409df97b5c2d2034b20ec1bd5fc2997e4c50932d
Chris Churas <churas.camera@gmail.com>
2023-05-19 11:44:52 -0700

    updated to pull needed info from __init__.py

--- setup.py
+++ setup.py
@@ -9,8 +9,17 @@ from setuptools import setup, find_packages
 
 with open(os.path.join('cellmaps_coembedding', '__init__.py')) as ver_file:
     for line in ver_file:
+        line = line.rstrip()
         if line.startswith('__version__'):
             version=re.sub("'", "", line[line.index("'"):])
+        elif line.startswith('__description__'):
+            desc = re.sub("'", "", line[line.index("'"):])
+        elif line.startswith('__repo_url__'):
+            repo_url = re.sub("'", "", line[line.index("'"):])
+        elif line.startswith('__author__'):
+            author = re.sub("'", "", line[line.index("'"):])
+        elif line.startswith('__email__'):
+            email = re.sub("'", "", line[line.index("'"):])
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -32,8 +41,8 @@ setup_requirements = [ ]
 test_requirements = [ ]
 
 setup(
-    author="Leah Schaffer",
-    author_email='lvschaffer@health.ucsd.edu',
+    author=author,
+    author_email=email,
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -44,11 +53,11 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
     ],
-    description="Python Boilerplate contains all the boilerplate you need to create a Python package with command line",
+    description=desc,
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
-    long_description_content_type = 'text/x-rst',
+    long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords='cellmaps_coembedding',
     name='cellmaps_coembedding',
@@ -59,6 +68,6 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/idekerlab/cellmaps_coembedding',
+    url=repo_url,
     version=version,
     zip_safe=False)
