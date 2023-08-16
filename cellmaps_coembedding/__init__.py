1817d1bbf21c8687d8c7a38bfa9a21d908b39a50
Chris Churas <churas.camera@gmail.com>
2023-08-16 16:50:30 -0700

    bumped version

--- cellmaps_coembedding/__init__.py
+++ cellmaps_coembedding/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for CM4AI Generate PPI."""
 
-__author__ = 'Ideker Lab CM4AI team'
+__author__ = 'Ideker Lab Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a5'
+__version__ = '0.1.0a6'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_coembedding'
 __description__ = 'A tool to generate coembeddings from IF image embeddings and PPI network embeddings'
