5a9570afccbaeb53d9e28e0a0a347d70c5f5f19e
Chris Churas <churas.camera@gmail.com>
2024-01-30 15:52:57 -0800

    bumped version

--- cellmaps_coembedding/__init__.py
+++ cellmaps_coembedding/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'Ideker Lab Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a10'
+__version__ = '0.1.0a11'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_coembedding'
 __description__ = 'A tool to generate coembeddings from IF image embeddings and PPI network embeddings'
 __computation_name__ = 'Merged Embedding'
