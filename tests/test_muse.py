c9453750f98d63c396dc45b417d5682e8ce31668
dzoanka <joanna.lenkiewicz@gmail.com>
2024-05-02 16:18:07 -0700

    Update tests for muse

--- tests/test_muse.py
+++ tests/test_muse.py
@@ -1,9 +1,12 @@
+import csv
+import os
+import shutil
+import tempfile
 import unittest
-from unittest.mock import patch
 
 import numpy as np
 
-from cellmaps_coembedding.muse_sc import make_matrix_from_labels, write_result_to_file, train_model, muse_fit_predict
+from cellmaps_coembedding.muse_sc import make_matrix_from_labels, write_result_to_file
 
 
 class TestMuse(unittest.TestCase):
@@ -19,13 +22,42 @@ class TestMuse(unittest.TestCase):
         result_matrix = make_matrix_from_labels(labels)
         np.testing.assert_array_equal(result_matrix, expected_matrix)
 
-    @patch('csv.writer')
-    def test_write_result_to_file(self, mock_writer):
-        # create temporary directory
-        # write to it
-        # check if content as expected
-        # delete temporary directory
-        pass
+    def test_single_cluster(self):
+        labels = np.array([0, 0, 0])
+        expected = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+        result = make_matrix_from_labels(labels)
+        np.testing.assert_array_equal(result, expected)
+
+    def test_multiple_clusters(self):
+        labels = np.array([0, 1, 0, 1, 2])
+        expected = np.array([
+            [1, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [1, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 1]
+        ])
+        result = make_matrix_from_labels(labels)
+        np.testing.assert_array_equal(result, expected)
+
+    def test_write_result_to_file(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            filepath = os.path.join(temp_dir, "results.tsv")
+            data = np.array([[1, 2], [3, 4]])
+            indexes = [0, 1]
+            write_result_to_file(filepath, data, indexes)
+            expected_rows = [
+                ['', '1'],
+                ['0', '1', '2'],
+                ['1', '3', '4']
+            ]
+            with open(filepath, 'r', newline='') as file:
+                reader = csv.reader(file, delimiter='\t')
+                for i, row in enumerate(reader):
+                    self.assertEqual(row, expected_rows[i])
+        finally:
+            shutil.rmtree(temp_dir)
 
 
 if __name__ == '__main__':
